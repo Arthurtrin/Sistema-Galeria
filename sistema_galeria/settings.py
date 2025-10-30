7cad057f92ab77938a2787a320110cbe57b55477
Arthur Trindade <arthurtrindade08102004@gmail.com>
2025-10-30 12:47:34 +0000

    teste deploy

--- sistema_galeria/settings.py
+++ sistema_galeria/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t%&%t4^8bnypo_-26@!zz(&+)2-v2044+1rh^h#bpi9ucyg#jp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['arthurtestesistemas.pythonanywhere.com', 'www.arthurtestesistemas.pythonanywhere.com']
 
 # Application definition
 
@@ -116,12 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Pasta onde o Django vai colocar os arquivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-import os
+# URL para acessar os arquivos estáticos
+STATIC_URL = '/static/'
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- sistema_galeria/wsgi.py
+++ sistema_galeria/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for sistema_galeria project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Caminho para a pasta do seu projeto
+project_home = '/home/arthurtestesistemas/Sistema-Galeria'
+if project_home not in sys.path:
+    sys.path.append(project_home)
 
+# Virtualenv (opcional, mas recomendado se estiver usando)
+activate_this = '/home/arthurtestesistemas/.virtualenvs/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Define as configurações do Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_galeria.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
